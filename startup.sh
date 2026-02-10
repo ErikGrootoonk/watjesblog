0e76840d29e51fd7dfb7167380418f1af8187b8a
ErikG <erik.grootoonk@gmail.com>
2026-02-10 19:39:35 +0100

    update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput
+./tailwindcss -i static/css/input.css -o static/css/style.css --minify
 python manage.py collectstatic --noinput
 gunicorn watjesblog.wsgi --bind=0.0.0.0:8000
